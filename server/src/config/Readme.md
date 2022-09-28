893f57e24aff769c7df281426fdcc1a8cb3664b7
xuxin666666 <3268154595@qq.com>
2022-09-28 11:52:28 +0800

    调整了一些配置

---
+++ server/src/config/Readme.md
@@ -0,0 +1,22 @@
+## 配置文件
+### default.json
+```json
+{
+    "version": "",
+    "server": {
+        "hostname": "",
+        "port": 0
+    },
+    "client": "", // 客户端网址，用来设置socket的cors
+    "env": "development", // 环境：开发环境，生产环境
+    "email" : { // 发邮件的配置
+        "host": "",
+        "port": 0,
+        "user": "",
+        "pass": "" // 授权码
+    }
+}
+```
+### development.json
+
+### production.json
--- server/src/pkg/sendEmail.ts
+++ server/src/pkg/sendEmail.ts
@@ -1,20 +1,20 @@
 import nodemailer from 'nodemailer'
+import config from 'config'
 
+
+const {host, port, user, pass} = config.get('email')
 
 const sendEmail = (email: string, code: string) => {
     return new Promise((resolve, reject) => {
-        // 授权码: zedchdmneosudbdc
+        // pass: 授权码
         let trans = nodemailer.createTransport({
-            host: 'smtp.qq.com',
-            port: 465,
+            host: host || 'smtp.qq.com',
+            port: port || 465,
             secure: true,
-            auth: {
-                user: '3268154595@qq.com',
-                pass: 'zedchdmneosudbdc'
-            }
+            auth: { user, pass }
         })
         let message = {
-            from: '3268154595@qq.com',
+            from: user,
             to: email,
             subject: '您好，外卖到了',
             text: `这是您的验证码：${code}，还有5分钟就凉了，请尽快食用`
